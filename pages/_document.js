7cfb2ba8169e8a39cfb82333b2860d037519d911
Joshua Mitchell <jlm346@txstate.edu>
2020-07-04 15:17:46 -0500

    fixed a small bug

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
     render() {
@@ -20,3 +20,5 @@ class MyDocument extends Document {
         );
     }
 }
+
+export default MyDocument;
